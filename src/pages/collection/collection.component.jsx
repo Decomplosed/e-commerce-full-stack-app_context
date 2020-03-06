d609c70a2e2920600ba646f09527b10ca2f146bd
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-06 13:21:25 +0100

    wrapped template inside Collections Consumer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -11,14 +11,16 @@ import './collection.styles.scss'
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
+    <CollectionContext.Consumer>
+      <div className='collection-page'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+          {items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+        </div>
       </div>
-    </div>
+    </CollectionContext.Consumer>
   )
 }
 
