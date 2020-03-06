924b9a0453e727025d75ebfbe9e10c27b2399786
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-06 13:20:33 +0100

    formatting fixes

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component'
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import { selectCollection } from '../../redux/shop/shop.selectors'
+import CollectionContext from '../../contexts/collections/collections.context'
 
-import './collection.styles.scss';
+import './collection.styles.scss'
 
 const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+  const { title, items } = collection
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -18,11 +19,11 @@ const CollectionPage = ({ collection }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+})
 
-export default connect(mapStateToProps)(CollectionPage);
+export default connect(mapStateToProps)(CollectionPage)
