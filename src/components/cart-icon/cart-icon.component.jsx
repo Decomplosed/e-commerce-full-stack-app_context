156cba092328702f7520a18bf979a0211093de00
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-07 16:26:43 +0100

    import CartContext

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
+import CartContext from '../../contexts/cart/cart.context'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
