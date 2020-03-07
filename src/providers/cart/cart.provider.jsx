f3d08d39b608177714b165d97f584a702d550bbd
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-07 17:01:32 +0100

    created CartContext

--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -1,3 +1,15 @@
 import React, { createContext, useState, useEffect } from 'react'
 
 import { addItemToCart, removeItemFromCart } from './cart.utils'
+
+const CartContext = createContext({
+  hidden: true,
+  toggleHidden: () => {},
+  cartItems: [],
+  addItem: () => {},
+  removeItem: () => {},
+  clearItemFromCart: () => {},
+  cartItemsCount: 0
+})
+
+export default CartContext
