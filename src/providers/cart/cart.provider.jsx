8e23b6a39dea868fee5f8d459d69bc41a414b089
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-07 17:08:04 +0100

    passed all functions as value to the Provider and export CartProvider

--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -16,9 +16,18 @@ const CartProvider = ({ children }) => {
   const [hidden, setHidden] = useState(true)
   const [cartItems, setCartItems] = useState([])
   const [cartItemsCount, setCartItemsCount] = useState(0)
+
+  const addItem = item => setCartItems(addItemToCart(cartItems, item))
+
   const toggleHidden = () => setHidden(!hidden)
 
-  return <CartContext.Provider>{children}</CartContext.Provider>
+  return (
+    <CartContext.Provider
+      value={{ hidden, toggleHidden, cartItems, addItem, cartItemsCount }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
 
-export default CartContext
+export default CartProvider
