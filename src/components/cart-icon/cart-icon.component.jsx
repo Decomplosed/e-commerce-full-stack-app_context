dea03bc0bb788218786bef5072126b313a3a5f73
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-08 11:59:52 +0100

    extracted cartItemsCount from CartContext

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
 const CartIcon = ({ itemCount }) => {
-  const { toggleHidden } = useContext(CartContext)
+  const { toggleHidden, cartItemsCount } = useContext(CartContext)
 
   return (
     <div className='cart-icon' onClick={toggleHidden}>
