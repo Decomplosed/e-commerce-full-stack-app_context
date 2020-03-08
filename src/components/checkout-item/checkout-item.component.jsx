662d8a566dfdd0e57f5db4e903a5d970fb57ce5c
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-08 11:54:33 +0100

    removed redux related imports

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react'
 import { connect } from 'react-redux'
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions'
-
 import { CartContext } from '../../providers/cart/cart.provider'
 import './checkout-item.styles.scss'
 
