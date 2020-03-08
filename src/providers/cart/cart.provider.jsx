fba7d24f068f3a873c924356a58d195de48be7f0
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-08 11:37:14 +0100

    passed removeItem to Provider as value

--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -24,7 +24,7 @@ const CartProvider = ({ children }) => {
 
   return (
     <CartContext.Provider
-      value={{ hidden, toggleHidden, cartItems, addItem, cartItemsCount }}
+      value={{ hidden, toggleHidden, cartItems, addItem, removeItem, cartItemsCount }}
     >
       {children}
     </CartContext.Provider>
