5161d6b053e63359f8a4f6353c0b4e5a644c31ca
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-08 11:38:57 +0100

    imported CartContext and useContext hook

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react'
+import { connect } from 'react-redux'
 
 import {
   clearItemFromCart,
   addItem,
   removeItem
-} from '../../redux/cart/cart.actions';
+} from '../../redux/cart/cart.actions'
 
-import './checkout-item.styles.scss';
+import { CartContext } from '../../providers/cart/cart.provider'
+import './checkout-item.styles.scss'
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem
   return (
     <div className='checkout-item'>
       <div className='image-container'>
@@ -31,16 +32,13 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
         &#10005;
       </div>
     </div>
-  );
-};
+  )
+}
 
 const mapDispatchToProps = dispatch => ({
   clearItem: item => dispatch(clearItemFromCart(item)),
   addItem: item => dispatch(addItem(item)),
   removeItem: item => dispatch(removeItem(item))
-});
+})
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(CheckoutItem);
+export default connect(null, mapDispatchToProps)(CheckoutItem)
