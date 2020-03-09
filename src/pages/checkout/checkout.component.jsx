55107de11c08b2963501d4729e5361b374503252
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-09 15:10:23 +0100

    implemented cartTotal

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -34,13 +34,13 @@ const CheckoutPage = () => {
       {cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className='total'>TOTAL: ${total}</div>
+      <div className='total'>TOTAL: ${cartTotal}</div>
       <div className='test-warning'>
         *Please use the following test credit card for payments*
         <br />
         4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
       </div>
-      <StripeCheckoutButton price={total} />
+      <StripeCheckoutButton price={cartTotal} />
     </div>
   )
 }
