b22e010fcee9c9dda6c66b92c7b15fb273f88d93
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-09 16:36:31 +0100

    imported DirectoryContext

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
+import MenuItem from '../menu-item/menu-item.component'
+import DirectoryContext from '../../contexts/directory/directory.context'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import { selectDirectorySections } from '../../redux/directory/directory.selectors'
-
-import MenuItem from '../menu-item/menu-item.component'
 
 import './directory.styles.scss'
 
